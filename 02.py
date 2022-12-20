f9b976b82183281e4c61d69231dcf343e4094348
Natalya Molchanova <molchanova.nathalie@yandex.ru>
2022-12-20 21:16:30 +0300

    add 02

---
+++ 02.py
@@ -0,0 +1,15 @@
+#Напишите программу, которая принимает на вход число N и выдает набор произведений чисел от 1 до N.
+
+
+def Fact(num):
+    list = []
+    result = 1
+    i = 1
+    while i <= num:
+        result = result * i
+        i=i+1
+        list.append(result)
+    return list
+
+n = int(input ('введите число '))
+print(Fact(n))
